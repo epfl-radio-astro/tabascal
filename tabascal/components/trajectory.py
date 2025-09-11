Fix RMSE plotting, use SGP4 trajectory for fixed orbit, add true calculation for specific ast signal.

tabascal/components/trajectory.py
@@ -1,7 +1,7 @@
-from tabsim.config import yaml_load
-from tabsim.tle import get_tles_by_id
+from tabsim.config import yaml_load  # type: ignore
+from tabsim.tle import get_tles_by_id  # type: ignore
 
-from tabsim.jax.coordinates import (
+from tabsim.jax.coordinates import (  # type: ignore
     itrf_to_uvw,
     itrf_to_xyz,
     kepler_orbit_many,
@@ -66,8 +66,8 @@ class PhaseCalculationRFI(Component):
 
     def build_set_params(self):
 
-        def set_params(state):
-            return state
+        def set_params(params):
+            return params
 
         return set_params
 
@@ -106,6 +106,7 @@ class FixedOrbit(Component):
         """All validation and error-prone operations here"""
         try:
             # Store only what's needed for forward computation
+            self.tles = config.tles
             self.elements = config.elements
             self.epoch_jd = config.epoch_jd
             self.n_rfi = config.n_rfi
@@ -171,11 +172,23 @@ class FixedOrbit(Component):
 
     def _compute_rfi_phase(self):
 
-        self.rfi_xyz = kepler_orbit_many(
-            self.times_jd_fine, self.epoch_jd, self.elements
-        )
+        from astropy.time import Time
+        from tabsim.tle import get_satellite_positions  # type: ignore
 
-        gsa = gmsa_from_jd(self.times_jd_fine) % 360
+        self.rfi_xyz = jnp.asarray(
+            get_satellite_positions(self.tles, list(self.times_jd_fine))
+        )
+        gsa = (
+            Time(self.times_jd_fine, format="jd")
+            .sidereal_time("mean", "greenwich")
+            .hour
+            * 15
+        )  # type: ignore
+
+        # self.rfi_xyz = kepler_orbit_many(
+        #     self.times_jd_fine, self.epoch_jd, self.elements
+        # )
+        # gsa = gmsa_from_jd(self.times_jd_fine) % 360
         gh0 = (gsa - self.phase_centre["ra"]) % 360
 
         self.ants_xyz = jnp.transpose(itrf_to_xyz(self.ants_itrf, gsa), axes=(1, 0, 2))
@@ -185,13 +198,13 @@ class FixedOrbit(Component):
 
         self.rfi_phase = get_rfi_phase(
             self.rfi_xyz, self.ants_uvw, self.ants_xyz, self.freqs_fine
-        )  # [:, :, 0, :]
+        )
 
     def _set_outputs(self):
 
         self.state_outputs = {
-            "rfi_xyz": jnp.zeros((self.n_rfi, self.n_time_fine, 3)),
-            "rfi_phase": jnp.zeros((self.n_rfi, self.n_ant, self.n_time_fine)),
+            "rfi_xyz": self.rfi_xyz,
+            "rfi_phase": self.rfi_phase,
         }
 
     def _validate_dimensions(self):

tabascal/plot.py
@@ -197,11 +197,21 @@ def plot_predictions(
     save_dir: str = "plots/",
 ):
 
+    rmse_ast = jnp.sqrt(
+        jnp.mean(jnp.abs(pred["vis_ast"] - truth["vis_ast"]) ** 2, axis=(0, 1))
+    )
+    rmse_rfi = jnp.sqrt(
+        jnp.mean(jnp.abs(pred["vis_rfi"] - truth["vis_rfi"]) ** 2, axis=(0, 1))
+    )
+    rmse_gains = jnp.sqrt(
+        jnp.mean(jnp.abs(pred["gains"] - truth["gains"]) ** 2, axis=(0, 1))
+    )
+
     plot_complex_real_imag(
         times=times,
-        param=pred["vis_ast"][:, 0],
-        true=truth["vis_ast"][:, 0],
-        rmse=pred["rmse_ast"],
+        param=pred["vis_ast"],
+        true=truth["vis_ast"],
+        rmse=rmse_ast,
         name="Ast. Vis.",
         save_name=f"{model_name}_{type}_ast_vis",
         max_plots=max_plots,
@@ -210,9 +220,9 @@ def plot_predictions(
 
     plot_complex_amp_phase(
         times=times,
-        param=pred["vis_rfi"][:, 0],
-        true=truth["vis_rfi"][:, 0],
-        rmse=pred["rmse_rfi"],
+        param=pred["vis_rfi"],
+        true=truth["vis_rfi"],
+        rmse=rmse_rfi,
         name="RFI Vis.",
         save_name=f"{model_name}_{type}_rfi_vis",
         diff=False,  # True,
@@ -222,9 +232,9 @@ def plot_predictions(
 
     plot_complex_amp_phase(
         times=times,
-        param=pred["gains"][:, 0],
-        true=truth["gains"][:, 0],
-        rmse=pred["rmse_gains"],
+        param=pred["gains"],
+        true=truth["gains"],
+        rmse=rmse_gains,
         name="Gains",
         save_name=f"{model_name}_{type}_gains",
         diff=False,