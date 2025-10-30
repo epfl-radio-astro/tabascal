Fix antenna calculation function, add plot reference curve.

tabascal/components/ast_vis.py
@@ -679,6 +679,16 @@ class FourierTimeFreqGPAst(Component):
             self.pad_factors = [self.freq_pad_factor, self.time_pad_factor]
             self.ss_factors = [1, 1]
 
+            self.get_latent_pred = lambda Z: get_latent(
+                Z,
+                self.xs,
+                self.pad_factors,
+                self.p0,
+                self.k0s,
+                self.gammas,
+                self.pk_cutoff,
+            )
+
             # Do expensive setup operations once
             self._compute_gp_params()
             self._compute_prior_params()
@@ -803,19 +813,9 @@ class FourierTimeFreqGPAst(Component):
             (1, 2, 0),
         )
 
-        get_latent_pred = lambda Z: get_latent(
-            Z,
-            self.xs,
-            self.pad_factors,
-            self.p0,
-            self.k0s,
-            self.gammas,
-            self.pk_cutoff,
-        )
-
         # self.true_ast_k = vmap(get_latent_pred, (0,), 0)(vis_ast)
 
-        self.true_ast_k = jnp.array([get_latent_pred(vis) for vis in vis_ast])
+        self.true_ast_k = jnp.array([self.get_latent_pred(vis) for vis in vis_ast])
 
         self.true_ast_k_base = self.inv_transform(
             self.true_ast_k, self.sigma_ast_k, self.mu_ast_k
@@ -851,6 +851,14 @@ class FourierTimeFreqGPAst(Component):
             self.init_ast_k = self.mu_ast_k
         elif init_type == "truth":
             self.init_ast_k = self.true_ast_k
+        elif init_type == "ones":
+            # self.init_ast_k = jnp.ones(
+            #     (self.n_bl, self.n_k_freq_ast, self.n_k_time_ast), dtype=complex
+            # )
+            ones = jnp.ones((self.n_freq, self.n_time), dtype=complex)
+            self.init_ast_k = jnp.stack(
+                self.n_bl * [self.get_latent_pred(ones)], axis=0
+            )
         else:
             prior_sample = random.normal(
                 random.PRNGKey(1),

tabascal/components/rfi_signal.py
@@ -532,8 +532,8 @@ class FourierGPRFI(Component):
                     config.args["data"]["zarr_path"], config.args["data"]["data_col"]
                 )
 
-            # if config.args["rfi"]["init"] == "est":
-            #     self._estimate_params(config.fringe_freqs)
+            if config.args["rfi"]["init"] == "est":
+                self._estimate_params(config.args["rfi"]["est"])
 
             self._compute_init_params(config.args["rfi"]["init"])
 
@@ -661,6 +661,28 @@ class FourierGPRFI(Component):
 
         return base_params
 
+    def _estimate_params(self, est_path: str):
+
+        import numpy as np
+
+        rfi_A = np.sqrt(np.abs(np.load(est_path)[:, :, -1]))
+
+        self.est_rfi_k_A = jnp.array(
+            [
+                [
+                    self.get_latent_pred(
+                        rfi_A[i, None] * jnp.ones((self.n_freq, self.n_time))
+                    )
+                    for _ in range(self.n_ant)
+                ]
+                for i in range(self.n_rfi)
+            ]
+        )
+
+        self.est_rfi_k_A_base = self.inv_transform(
+            self.est_rfi_k_A, self.sigma_rfi_k, self.mu_rfi_k
+        )
+
     def _compute_true_params(self, zarr_path, data_col):
 
         xds = xr.open_zarr(zarr_path)
@@ -710,6 +732,9 @@ class FourierGPRFI(Component):
                     for _ in range(self.n_rfi)
                 ]
             )
+        elif init_type == "est":
+            print("Using estimate for rfi_k")
+            self.init_rfi_k = self.est_rfi_k_A
         else:
             print("Drawing sample from prior for rfi_k")
             prior_sample = random.normal(

tabascal/components/trajectory.py
@@ -351,7 +351,10 @@ def itrs_to_gcrs_sf(pos_itrs: Array, times_jd: Array) -> Array:
     t_sf = ts.ut1_jd(np.array(times_jd))
 
     pos_gcrs = jnp.stack(
-        [ITRSPosition(Distance(m=pos)).at(t_sf).position.m.T for pos in pos_itrs]
+        [
+            np.array(ITRSPosition(Distance(m=np.array(pos))).at(t_sf).position.m.T)
+            for pos in pos_itrs
+        ]
     )
 
     return pos_gcrs

tabascal/plot.py
@@ -39,7 +39,18 @@ def time_units(times: NDArray) -> tuple:
 
 
 def plot_comparison(
-    ax, times, mean1, mean2, std1, std2, true1, true2, rmse, diff=False
+    ax,
+    times,
+    mean1,
+    mean2,
+    std1,
+    std2,
+    true1,
+    true2,
+    true_std,
+    rmse,
+    diff=False,
+    ref_label="Truth",
 ):
 
     times, units = time_units(times)
@@ -59,7 +70,7 @@ def plot_comparison(
                 times, -2 * std2[i], 2 * std2[i], color="tab:orange", alpha=0.3
             )
         else:
-            a[1].plot(times, true1[i], label="True")
+            a[1].errorbar(times, true1[i], true_std, label=ref_label)
             a[1].plot(times, mean1[i], label="Estimate")
             a[1].fill_between(
                 times,
@@ -75,7 +86,7 @@ def plot_comparison(
                 color="tab:orange",
                 alpha=0.3,
             )
-            a[2].plot(times, true2[i], label="True")
+            a[2].errorbar(times, true2[i], true_std, label=ref_label)
             a[2].plot(times, mean2[i], label="Estimate")
             a[2].fill_between(
                 times,
@@ -106,6 +117,8 @@ def plot_complex_real_imag(
     name: str,
     save_name: Optional[str] = None,
     diff: bool = False,
+    ref_noise: float = 0,
+    ref_label: str = "Truth",
     max_plots: int = 10,
     save_dir: str = "plots/",
 ):
@@ -127,7 +140,18 @@ def plot_complex_real_imag(
     ax[0, 2].set_title(f"{name} Imag")
 
     plot_comparison(
-        ax, times, mean_r, mean_i, std_r, std_i, true.real, true.imag, rmse, diff=diff
+        ax,
+        times,
+        mean_r,
+        mean_i,
+        std_r,
+        std_i,
+        true.real,
+        true.imag,
+        ref_noise,
+        rmse,
+        diff=diff,
+        ref_label=ref_label,
     )
 
     if save_name is not None:
@@ -147,6 +171,8 @@ def plot_complex_amp_phase(
     name: str,
     save_name: Optional[str] = None,
     diff: bool = False,
+    ref_noise: float = 0,
+    ref_label: str = "Truth",
     max_plots: int = 10,
     save_dir: str = "plots/",
 ):
@@ -175,8 +201,10 @@ def plot_complex_amp_phase(
         std_phase,
         jnp.abs(true),
         jnp.rad2deg(jnp.angle(true)),
+        ref_noise,
         rmse,
         diff=diff,
+        ref_label=ref_label,
     )
 
     if save_name is not None:
@@ -191,30 +219,34 @@ def plot_complex_amp_phase(
 def plot_predictions(
     times,
     pred,
-    truth,
+    ref,
+    ref_noise: dict[str, float] = {"vis_ast": 0, "vis_rfi": 0, "gains": 0},
     type: str = "",
     model_name: str = "",
     max_plots: int = 10,
     save_dir: str = "plots/",
+    ref_label: dict[str, str] = {"vis_ast": "", "vis_rfi": "", "gains": ""},
 ):
 
     rmse_ast = jnp.sqrt(
-        jnp.mean(jnp.abs(pred["vis_ast"] - truth["vis_ast"]) ** 2, axis=(0, 1))
+        jnp.mean(jnp.abs(pred["vis_ast"] - ref["vis_ast"]) ** 2, axis=(0, 1))
     )
     rmse_rfi = jnp.sqrt(
-        jnp.mean(jnp.abs(pred["vis_rfi"] - truth["vis_rfi"]) ** 2, axis=(0, 1))
+        jnp.mean(jnp.abs(pred["vis_rfi"] - ref["vis_rfi"]) ** 2, axis=(0, 1))
     )
     rmse_gains = jnp.sqrt(
-        jnp.mean(jnp.abs(pred["gains"] - truth["gains"]) ** 2, axis=(0, 1))
+        jnp.mean(jnp.abs(pred["gains"] - ref["gains"]) ** 2, axis=(0, 1))
     )
 
     plot_complex_real_imag(
         times=times,
         param=pred["vis_ast"][:, :, 0],
-        true=truth["vis_ast"][:, 0],
+        true=ref["vis_ast"][:, 0],
         rmse=rmse_ast,
         name="Ast. Vis.",
         save_name=f"{model_name}_{type}_ast_vis",
+        ref_noise=ref_noise["vis_ast"],
+        ref_label=ref_label["vis_ast"],
         max_plots=max_plots,
         save_dir=save_dir,
     )
@@ -222,10 +254,12 @@ def plot_predictions(
     plot_complex_amp_phase(
         times=times,
         param=pred["vis_rfi"][:, :, 0],
-        true=truth["vis_rfi"][:, 0],
+        true=ref["vis_rfi"][:, 0],
         rmse=rmse_rfi,
         name="RFI Vis.",
         save_name=f"{model_name}_{type}_rfi_vis",
+        ref_noise=ref_noise["vis_rfi"],
+        ref_label=ref_label["vis_rfi"],
         diff=False,  # True,
         max_plots=max_plots,
         save_dir=save_dir,
@@ -234,10 +268,12 @@ def plot_predictions(
     plot_complex_amp_phase(
         times=times,
         param=pred["gains"][:, :, 0],
-        true=truth["gains"][:, 0],
+        true=ref["gains"][:, 0],
         rmse=rmse_gains,
         name="Gains",
         save_name=f"{model_name}_{type}_gains",
+        ref_noise=ref_noise["gains"],
+        ref_label=ref_label["gains"],
         diff=False,
         max_plots=max_plots,
         save_dir=save_dir,

tabascal/scripts/run_tabascal.py
@@ -5,6 +5,8 @@ import os
 import sys
 import yaml
 
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"  # Remove SoL warnings
+# export TF_CPP_MIN_LOG_LEVEL=2
 os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = (
     "false"  # Disable GPU Memory Preallocation
 )
@@ -48,8 +50,6 @@ def tabascal_subtraction(
     config: dict,
     sim_dir: str,
     ms_path: Optional[str] = None,
-    spacetrack_path: Optional[str] = None,
-    norad_ids: list = [],
     suffix: str = "",
 ):
 
@@ -58,7 +58,7 @@ def tabascal_subtraction(
 
     run_id = datetime.now().strftime("%m-%d-%YT%H:%M:%S")
 
-    log_path = f"log_tab_{run_id}.txt"
+    log_path = f"log_tab{suffix}_{run_id}.txt"
     log = open(log_path, "w")
     backup = sys.stdout
     sys.stdout = Tee(sys.stdout, log)
@@ -162,15 +162,7 @@ def tabascal_subtraction(
     print(f"log_l : {nlog_l:.3e}")
     print(f"log_p : {nlog_p:.3e}")
 
-    init_state = model.forward(model.init_params, model.state)
-
-    # truth = {
-    #     "vis_rfi": init_state["vis_rfi"],
-    #     "vis_ast": init_state["vis_ast"],
-    #     "gains": init_state["gains"],
-    # }
-
-    truth = {
+    ref = {
         "vis_rfi": jnp.nan
         * jnp.zeros(
             (tab_config.n_bl, tab_config.n_freq, tab_config.n_time), dtype=complex
@@ -184,9 +176,43 @@ def tabascal_subtraction(
             (tab_config.n_ant, tab_config.n_freq, tab_config.n_time), dtype=complex
         ),
     }
+    ref_label = {
+        "vis_rfi": "",
+        "vis_ast": "",
+        "gains": "",
+    }
+    ref_noise = {
+        "vis_rfi": 0.0,
+        "vis_ast": 0.0,
+        "gains": 0.0,
+    }
+
+    init_state = model.forward(model.init_params, model.state)
+
+    # ref["vis_rfi"] = init_state["vis_rfi"]
+    # ref_label["vis_rfi"] = "Initial"
+    # ref_noise["vis_rfi"] = tab_config.noise
+
+    # ref["vis_ast"] = init_state["vis_ast"]
+    # ref_label["vis_ast"] = "Initial"
+    # ref_noise["vis_ast"] = tab_config.noise
+
+    # ref["gains"] = init_state["gains"]
+    # ref_label["gains"] = "Initial"
+    # ref_noise["gains"] = 0.0
+
+    ref["vis_rfi"] = tab_config.vis_obs
+    ref_label["vis_rfi"] = "Vis Obs"
+    ref_noise["vis_rfi"] = tab_config.noise
+
+    ref["vis_ast"] = tab_config.vis_obs
+    ref_label["vis_ast"] = "Vis Obs"
+    ref_noise["vis_ast"] = tab_config.noise
 
     if config["plots"]["init"]:
-        plot_init(tab_config, init_pred, truth, model_name, plot_dir)
+        plot_init(
+            tab_config, init_pred, ref, ref_noise, model_name, plot_dir, ref_label
+        )
 
     ### Check and Plot Model at true parameters
     # if config["plots"]["truth"]:
@@ -214,10 +240,12 @@ def tabascal_subtraction(
         plot_prior(
             tab_config,
             prob_model,
-            truth,
+            ref,
+            ref_noise,
             model_name,
             subkey,
             plot_dir,
+            ref_label,
         )
 
     # ### Run MCMC Inference
@@ -247,14 +275,16 @@ def tabascal_subtraction(
         vi_params, rchi2 = run_opt(
             tab_config,
             prob_model,
-            truth,
+            ref,
+            ref_noise,
             model_name,
-            subkeys,
+            subkeys,  # type: ignore
             model.init_params,
             plot_dir,
             ms_path,
             map_path,
             params_path,
+            ref_label,
         )
 
         opt_params = {
@@ -278,7 +308,7 @@ def tabascal_subtraction(
 
     mem_i = save_memory(mem_dir, mem_i)
 
-    max_fisher_time = 30 * 60  # seconds
+    # max_fisher_time = 30 * 60  # seconds
 
     # ### Run Fisher Covariance Prediction
     # key, *subkeys = random.split(key, 3)
@@ -317,7 +347,7 @@ def tabascal_subtraction(
     os.remove(log_path)
     sys.stdout = backup
 
-    with open(os.path.join(plot_dir, f"tab_config_{run_id}.yaml"), "w") as fp:
+    with open(os.path.join(plot_dir, f"tab_config{suffix}_{run_id}.yaml"), "w") as fp:
         yaml.dump(config, fp)
 
 
@@ -333,9 +363,6 @@ def main():
     )
     parser.add_argument("-ms", "--ms_path", help="Path to Measurement Set.")
     parser.add_argument(
-        "-np", "--norad_path", help="Path to text file containing NORAD IDs to include."
-    )
-    parser.add_argument(
         "-st", "--spacetrack", help="Path to Space-Track login details."
     )
     parser.add_argument("-sx", "--suffix", default="", help="Image name suffix.")
@@ -343,18 +370,13 @@ def main():
     sim_dir = args.sim_dir
     conf_path = args.config
     spacetrack_path = args.spacetrack
-    norad_path = args.norad_path
-    if sim_dir:
-        norad_path = os.path.join(sim_dir, "input_data/norad_ids.yaml")
-    else:
-        sim_dir = os.path.split(args.ms_path)[0]
-
-    if norad_path:
-        norad_ids = [int(x) for x in np.atleast_1d(np.loadtxt(norad_path))]
-    else:
-        norad_ids = []
 
     config = load_config(conf_path, config_type="tab")
+
+    if not sim_dir and args.ms_path:
+        sim_dir = os.path.split(args.ms_path)[0]
+    elif not sim_dir:
+        sim_dir = config["data"]["sim_dir"]
 
     config_st_path = config["satellites"]["spacetrack_path"]
     if spacetrack_path:
@@ -364,9 +386,7 @@ def main():
         config["satellites"]["spacetrack_path"] = config_st_path
         spacetrack_path = config_st_path
 
-    tabascal_subtraction(
-        config, sim_dir, args.ms_path, spacetrack_path, norad_ids, args.suffix
-    )
+    tabascal_subtraction(config, sim_dir, args.ms_path, args.suffix)
 
 
 if __name__ == "__main__":

tabascal/tab_tools.py
@@ -127,22 +127,22 @@ def read_ms(
     correlations = {"xx": 0, "xy": 1, "yx": 2, "yy": 3}
     corr_idx = correlations[corr]
 
-    xds = xds_from_ms(ms_path)[0]
-    xds_ant = xds_from_table(ms_path + "::ANTENNA")[0]
-    xds_spec = xds_from_table(ms_path + "::SPECTRAL_WINDOW")[0]
-    xds_src = xds_from_table(ms_path + "::SOURCE")[0]
+    xds = xds_from_ms(ms_path)[0]  # type: ignore
+    xds_ant = xds_from_table(ms_path + "::ANTENNA")[0]  # type: ignore
+    xds_spec = xds_from_table(ms_path + "::SPECTRAL_WINDOW")[0]  # type: ignore
+    xds_src = xds_from_table(ms_path + "::SOURCE")[0]  # type: ignore
 
-    ants_itrf = jnp.array(xds_ant.POSITION.data.compute())
+    ants_itrf = jnp.array(xds_ant.POSITION.data.compute())  # type: ignore
 
     n_ant = ants_itrf.shape[0]
     n_time = len(np.unique(xds.TIME.data.compute()))
     n_bl = xds.DATA.data.shape[0] // n_time
     n_freq, n_corr = xds.DATA.data.shape[1:]
 
-    freqs = jnp.array(xds_spec.CHAN_FREQ.data[0].compute())
-    int_time = xds.INTERVAL.data[0].compute()
+    freqs = jnp.array(xds_spec.CHAN_FREQ.data[0].compute())  # type: ignore
+    int_time = xds.INTERVAL.data[0].compute()  # type: ignore
 
-    times_mjd = jnp.array(xds.TIME.data.reshape(n_time, n_bl)[:, 0].compute())
+    times_mjd = jnp.array(xds.TIME.data.reshape(n_time, n_bl)[:, 0].compute())  # type: ignore
     if times_mjd[1] - times_mjd[0] > 0.5:
         times_mjd = times_mjd / (24 * 3600)
 
@@ -167,7 +167,7 @@ def read_ms(
         **{
             key: val
             for key, val in zip(
-                ["ra", "dec"], jnp.rad2deg(xds_src.DIRECTION.data[0].compute())
+                ["ra", "dec"], jnp.rad2deg(xds_src.DIRECTION.data[0].compute())  # type: ignore
             )
         },
         "n_freq": n_freq,
@@ -175,24 +175,24 @@ def read_ms(
         "n_time": n_time,
         "n_ant": n_ant,
         "n_bl": n_bl,
-        "dish_d": xds_ant.DISH_DIAMETER.data[0].compute(),
+        "dish_d": xds_ant.DISH_DIAMETER.data[0].compute(),  # type: ignore
         "times_mjd": times_mjd,
         "times": times,
         "int_time": int_time,
         "freqs": freqs[chans],
         "ants_itrf": ants_itrf,
-        "uvw": jnp.array(xds.UVW.data.reshape(n_time, n_bl, 3).compute()),
+        "uvw": jnp.array(xds.UVW.data.reshape(n_time, n_bl, 3).compute()),  # type: ignore
         "vis_obs": jnp.transpose(
             jnp.array(
-                xds[data_col]
+                xds[data_col]  # type: ignore
                 .data.reshape(n_time, n_bl, n_freq, n_corr)
                 .compute()[:, :, chans, corr_idx]
             ),
             (1, 2, 0),
         ),
-        "noise": jnp.array(xds.SIGMA.data.mean().compute()),
-        "a1": jnp.array(xds.ANTENNA1.data.reshape(n_time, n_bl)[0, :].compute()),
-        "a2": jnp.array(xds.ANTENNA2.data.reshape(n_time, n_bl)[0, :].compute()),
+        "noise": jnp.array(xds.SIGMA.data.mean().compute()),  # type: ignore
+        "a1": jnp.array(xds.ANTENNA1.data.reshape(n_time, n_bl)[0, :].compute()),  # type: ignore
+        "a2": jnp.array(xds.ANTENNA2.data.reshape(n_time, n_bl)[0, :].compute()),  # type: ignore
     }
 
     return data
@@ -325,7 +325,15 @@ def init_predict(
     return init_pred
 
 
-def plot_init(tab_config, init_pred: dict, truth: dict, model_name: str, plot_dir: str):
+def plot_init(
+    tab_config,
+    init_pred: dict,
+    ref: dict,
+    ref_noise: dict[str, float],
+    model_name: str,
+    plot_dir: str,
+    ref_label: dict[str, str],
+):
 
     start = datetime.now()
     print()
@@ -333,11 +341,13 @@ def plot_init(tab_config, init_pred: dict, truth: dict, model_name: str, plot_di
     plot_predictions(
         times=tab_config.times,
         pred=init_pred,
-        truth=truth,
+        ref=ref,
+        ref_noise=ref_noise,
         type="init",
         model_name=model_name,
         max_plots=10,
         save_dir=plot_dir,
+        ref_label=ref_label,
     )
     # if get_truth_conditional(config):
 
@@ -354,10 +364,12 @@ def plot_init(tab_config, init_pred: dict, truth: dict, model_name: str, plot_di
 def plot_prior(
     tab_config,
     prob_model: Callable,
-    truth: dict,
+    ref: dict,
+    ref_noise: dict[str, float],
     model_name: str,
     subkey: jax.Array,
     plot_dir: str,
+    ref_label: dict[str, str],
 ):
 
     start = datetime.now()
@@ -370,11 +382,13 @@ def plot_prior(
     plot_predictions(
         times=tab_config.times,
         pred=prior_pred,
-        truth=truth,
+        ref=ref,
+        ref_noise=ref_noise,
         type="prior",
         model_name=model_name,
         max_plots=10,
         save_dir=plot_dir,
+        ref_label=ref_label,
     )
     print()
     print(f"Prior Plot Time : {datetime.now() - start}")
@@ -384,7 +398,8 @@ def plot_prior(
 def run_opt(
     tab_config,
     prob_model: Callable,
-    truth: dict[str, jax.Array],
+    ref: dict[str, jax.Array],
+    ref_noise: dict[str, float],
     model_name: str,
     subkeys: jax.Array,
     init_params: dict,
@@ -392,6 +407,7 @@ def run_opt(
     ms_path,
     map_path,
     params_path,
+    ref_label: dict[str, str],
 ):
 
     guides = {
@@ -432,11 +448,13 @@ def run_opt(
     plot_predictions(
         tab_config.times,
         pred=vi_pred,
-        truth=truth,
+        ref=ref,
+        ref_noise=ref_noise,
         type=tab_config.args["opt"]["guide"],
         model_name=model_name,
         max_plots=10,
         save_dir=plot_dir,
+        ref_label=ref_label,
     )
     print()
     print(f"Optimize Plot Time : {datetime.now() - start}")