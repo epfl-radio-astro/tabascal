0d993a5db9ebdad27945670a057c34e4a7383696
Simon Frasch <simon.frasch@cscs.ch>
2026-05-19 17:29:03 +0200

    simplify jvp kernel

--- tabascal/components/ffi/rfi_jvp_kernel_gpu.cu
+++ tabascal/components/ffi/rfi_jvp_kernel_gpu.cu
@@ -19,34 +19,25 @@ namespace ffi = xla::ffi;
 namespace tabascal {
 namespace gpu {
 
-template <int BLOCK_SIZE, int WARP_SIZE, typename INT_T>
+template <int BLOCK_SIZE, typename INT_T>
 __global__ void __launch_bounds__(BLOCK_SIZE)
-    rfi_jvp_kernel(INT_T n_int_f, INT_T n_int_t,
-                   Tensor1D<const int *, INT_T> a1,
+    rfi_jvp_kernel(double scale, Tensor1D<const int *, INT_T> a1,
                    Tensor1D<const int *, INT_T> a2,
-                   Tensor5D<const cuDoubleComplex *, INT_T> rfi_amp_fine,
-                   Tensor5D<const cuDoubleComplex *, INT_T> rfi_amp_fine_grad,
-                   Tensor5D<const double *, INT_T> rfi_phase,
-                   Tensor5D<const double *, INT_T> rfi_phase_grad,
+                   Tensor4D<const cuDoubleComplex *, INT_T> rfi_amp_fine,
+                   Tensor4D<const cuDoubleComplex *, INT_T> rfi_amp_fine_grad,
+                   Tensor4D<const double *, INT_T> rfi_phase,
+                   Tensor4D<const double *, INT_T> rfi_phase_grad,
                    Tensor3D<cuDoubleComplex *, INT_T> rfi_grad) {
 
-  static_assert(BLOCK_SIZE % WARP_SIZE == 0);
-  constexpr int WARPS_PER_BLOCK = BLOCK_SIZE / WARP_SIZE;
+  using BlockReduce_t = cub::BlockReduce<double, BLOCK_SIZE>;
 
-  using WarpReduce_t = cub::WarpReduce<double>;
+  __shared__ typename BlockReduce_t::TempStorage temp_storage_1;
+  __shared__ typename BlockReduce_t::TempStorage temp_storage_2;
 
-  __shared__ typename WarpReduce_t::TempStorage temp_storage_1[WARPS_PER_BLOCK];
-  __shared__ typename WarpReduce_t::TempStorage temp_storage_2[WARPS_PER_BLOCK];
-
-  const int warp_id = threadIdx.x / WARP_SIZE;
-  const int lane_id = threadIdx.x % WARP_SIZE;
-
-  const int n_warps_global = gridDim.x * WARPS_PER_BLOCK;
-
-  // rfi_amp_fine layout: (n_ant, n_freq, n_time, n_rfi, n_int_f * n_int_t)
+  // rfi_amp_fine layout: (n_ant, n_freq, n_time, n_rfi * n_int_f * n_int_t)
   const auto n_freq = rfi_amp_fine.shape[1];
   const auto n_time = rfi_amp_fine.shape[2];
-  const auto n_rfi = rfi_amp_fine.shape[3];
+  const auto n_reduce = rfi_amp_fine.shape[3];
   const auto n_bl = a1.shape[0];
 
   assert(a1.shape[0] == a2.shape[0]);
@@ -55,11 +46,9 @@ __global__ void __launch_bounds__(BLOCK_SIZE)
   assert(rfi_phase.shape[1] == rfi_amp_fine.shape[1]);
   assert(rfi_phase.shape[2] == rfi_amp_fine.shape[2]);
   assert(rfi_phase.shape[3] == rfi_amp_fine.shape[3]);
-  assert(rfi_phase.shape[4] == rfi_amp_fine.shape[4]);
   assert(rfi_grad.shape[1] == n_freq);
   assert(rfi_grad.shape[2] == n_time);
 
-  const double n_int_inv = 1.f / double(n_int_t * n_int_f);
 
   for (INT_T i_bl = blockIdx.y; i_bl < n_bl; i_bl += gridDim.y) {
     INT_T i_a1 = a1(i_bl);
@@ -67,76 +56,70 @@ __global__ void __launch_bounds__(BLOCK_SIZE)
 
     for (INT_T i_f = blockIdx.z; i_f < n_freq; i_f += gridDim.z) {
 
-      // warp reduction
-      for (INT_T i_t = blockIdx.x * WARPS_PER_BLOCK + warp_id; i_t < n_time;
-           i_t += n_warps_global) {
+      for (INT_T i_t = blockIdx.x; i_t < n_time; i_t += gridDim.x) {
         cuDoubleComplex sum{0, 0};
 
-        for (INT_T i_rfi = 0; i_rfi < n_rfi; ++i_rfi) {
+        const auto ptr_rfi_amp_1 = &rfi_amp_fine(i_a1, i_f, i_t, 0);
+        const auto ptr_rfi_amp_2 = &rfi_amp_fine(i_a2, i_f, i_t, 0);
 
-          for (INT_T i_int_f = 0; i_int_f < n_int_f; ++i_int_f) {
+        const auto ptr_rfi_amp_grad_1 = &rfi_amp_fine_grad(i_a1, i_f, i_t, 0);
+        const auto ptr_rfi_amp_grad_2 = &rfi_amp_fine_grad(i_a2, i_f, i_t, 0);
 
-            for (INT_T i_int_t = lane_id; i_int_t < n_int_t;
-                 i_int_t += WARP_SIZE) {
+        const auto ptr_rfi_phase_1 = &rfi_phase(i_a1, i_f, i_t, 0);
+        const auto ptr_rfi_phase_2 = &rfi_phase(i_a2, i_f, i_t, 0);
 
-              const INT_T i_int_ft = i_int_f * n_int_t + i_int_t;
+        const auto ptr_rfi_phase_grad_1 = &rfi_phase_grad(i_a1, i_f, i_t, 0);
+        const auto ptr_rfi_phase_grad_2 = &rfi_phase_grad(i_a2, i_f, i_t, 0);
 
-              const auto val_rfi_amp_1 =
-                  rfi_amp_fine(i_a1, i_f, i_t, i_rfi, i_int_ft);
-              const auto val_rfi_amp_2 =
-                  rfi_amp_fine(i_a2, i_f, i_t, i_rfi, i_int_ft);
+        // block reduction
+        for (INT_T i_red = threadIdx.x; i_red < n_reduce; i_red += BLOCK_SIZE) {
 
-              const auto val_rfi_amp_grad_1 =
-                  rfi_amp_fine_grad(i_a1, i_f, i_t, i_rfi, i_int_ft);
-              const auto val_rfi_amp_grad_2 =
-                  rfi_amp_fine_grad(i_a2, i_f, i_t, i_rfi, i_int_ft);
+          const auto val_rfi_amp_1 = ptr_rfi_amp_1[i_red];
+          const auto val_rfi_amp_2 = ptr_rfi_amp_2[i_red];
 
-              const auto val_rfi_phase_1 =
-                  rfi_phase(i_a1, i_f, i_t, i_rfi, i_int_ft);
-              const auto val_rfi_phase_2 =
-                  rfi_phase(i_a2, i_f, i_t, i_rfi, i_int_ft);
+          const auto val_rfi_amp_grad_1 = ptr_rfi_amp_grad_1[i_red];
+          const auto val_rfi_amp_grad_2 = ptr_rfi_amp_grad_2[i_red];
 
-              const auto val_rfi_phase_grad_1 =
-                  rfi_phase_grad(i_a1, i_f, i_t, i_rfi, i_int_ft);
-              const auto val_rfi_phase_grad_2 =
-                  rfi_phase_grad(i_a2, i_f, i_t, i_rfi, i_int_ft);
+          const auto val_rfi_phase_1 = ptr_rfi_phase_1[i_red];
+          const auto val_rfi_phase_2 = ptr_rfi_phase_2[i_red];
 
-              cuDoubleComplex e_val;
-              sincos(val_rfi_phase_1 - val_rfi_phase_2, &e_val.y, &e_val.x);
+          const auto val_rfi_phase_grad_1 = ptr_rfi_phase_grad_1[i_red];
+          const auto val_rfi_phase_grad_2 = ptr_rfi_phase_grad_2[i_red];
 
-              // const auto g1 =
-              //     val_e * (val_rfi_amp_grad_1 * std::conj(val_rfi_amp_2) +
-              //              val_rfi_amp_1 * std::conj(val_rfi_amp_grad_2));
+          cuDoubleComplex e_val;
+          sincos(val_rfi_phase_1 - val_rfi_phase_2, &e_val.y, &e_val.x);
 
-              auto g1 = cuCmul(val_rfi_amp_grad_1, cuConj(val_rfi_amp_2));
+          // const auto g1 =
+          //     val_e * (val_rfi_amp_grad_1 * std::conj(val_rfi_amp_2) +
+          //              val_rfi_amp_1 * std::conj(val_rfi_amp_grad_2));
 
-              g1 =
-                  cuCadd(g1, cuCmul(val_rfi_amp_1, cuConj(val_rfi_amp_grad_2)));
+          auto g1 = cuCmul(val_rfi_amp_grad_1, cuConj(val_rfi_amp_2));
 
-              g1 = cuCmul(g1, e_val);
+          g1 = cuCadd(g1, cuCmul(val_rfi_amp_1, cuConj(val_rfi_amp_grad_2)));
 
-              // const auto g2 = val_e * std::complex<double>(0, 1) *
-              //                 (val_rfi_phase_grad_1 - val_rfi_phase_grad_2) *
-              //                 val_rfi_amp_1 * std::conj(val_rfi_amp_2);
+          g1 = cuCmul(g1, e_val);
 
-              auto g2 = cuDoubleComplex{
-                  val_rfi_phase_grad_1 - val_rfi_phase_grad_2, 0};
+          // const auto g2 = val_e * std::complex<double>(0, 1) *
+          //                 (val_rfi_phase_grad_1 - val_rfi_phase_grad_2) *
+          //                 val_rfi_amp_1 * std::conj(val_rfi_amp_2);
 
-              g2 = cuCmul(g2, cuCmul(val_rfi_amp_1, cuConj(val_rfi_amp_2)));
+          auto g2 =
+              cuDoubleComplex{val_rfi_phase_grad_1 - val_rfi_phase_grad_2, 0};
 
-              g2 = cuCmul(g2, cuDoubleComplex{-e_val.y, e_val.x});
+          g2 = cuCmul(g2, cuCmul(val_rfi_amp_1, cuConj(val_rfi_amp_2)));
 
-              sum = cuCadd(sum, cuCadd(g1, g2));
-            }
-          }
+          g2 = cuCmul(g2, cuDoubleComplex{-e_val.y, e_val.x});
+
+          sum = cuCadd(sum, cuCadd(g1, g2));
         }
-        sum.x = WarpReduce_t(temp_storage_1[warp_id]).Sum(sum.x);
-        sum.y = WarpReduce_t(temp_storage_2[warp_id]).Sum(sum.y);
-        __syncwarp();
 
-        if (lane_id == 0) {
-          sum.x *= n_int_inv;
-          sum.y *= n_int_inv;
+        sum.x = BlockReduce_t(temp_storage_1).Sum(sum.x);
+        sum.y = BlockReduce_t(temp_storage_2).Sum(sum.y);
+        __syncthreads(); // required for reuse of temp_storage
+
+        if (threadIdx.x == 0) {
+          sum.x *= scale;
+          sum.y *= scale;
 
           rfi_grad(i_bl, i_f, i_t) = sum;
         }
@@ -198,32 +181,35 @@ ffi::Error calc_rfi_jvp_gpu_dispatch(
 
   // rfi_amp_fine / rfi_phase (and their grads) layout:
   //   (n_ant, n_freq, n_time, n_rfi, n_int_freq, n_int_time)
-  // Collapse innermost (n_int_freq, n_int_time) into one for kernel indexing.
+  // Collapse the three innermost contiguous dims (n_rfi, n_int_freq,
+  // n_int_time) into one for kernel indexing.
   Tensor1D<const int *, INT_T> a1_tensor(a1.typed_data(), a1.dimensions()[0]);
   Tensor1D<const int *, INT_T> a2_tensor(a2.typed_data(), a2.dimensions()[0]);
-  Tensor5D<const cuDoubleComplex *, INT_T> rfi_amp_fine_tensor(
+  Tensor4D<const cuDoubleComplex *, INT_T> rfi_amp_fine_tensor(
       (const cuDoubleComplex *)rfi_amp_fine.typed_data(),
       rfi_amp_fine.dimensions()[0], rfi_amp_fine.dimensions()[1],
-      rfi_amp_fine.dimensions()[2], rfi_amp_fine.dimensions()[3],
-      rfi_amp_fine.dimensions()[4] * rfi_amp_fine.dimensions()[5]);
+      rfi_amp_fine.dimensions()[2],
+      rfi_amp_fine.dimensions()[3] * rfi_amp_fine.dimensions()[4] *
+          rfi_amp_fine.dimensions()[5]);
 
-  Tensor5D<const cuDoubleComplex *, INT_T> rfi_amp_fine_grad_tensor(
+  Tensor4D<const cuDoubleComplex *, INT_T> rfi_amp_fine_grad_tensor(
       (const cuDoubleComplex *)rfi_amp_fine_grad.typed_data(),
       rfi_amp_fine_grad.dimensions()[0], rfi_amp_fine_grad.dimensions()[1],
-      rfi_amp_fine_grad.dimensions()[2], rfi_amp_fine_grad.dimensions()[3],
-      rfi_amp_fine_grad.dimensions()[4] * rfi_amp_fine_grad.dimensions()[5]);
+      rfi_amp_fine_grad.dimensions()[2],
+      rfi_amp_fine_grad.dimensions()[3] * rfi_amp_fine_grad.dimensions()[4] *
+          rfi_amp_fine_grad.dimensions()[5]);
 
-  Tensor5D<const double *, INT_T> rfi_phase_tensor(
+  Tensor4D<const double *, INT_T> rfi_phase_tensor(
       rfi_phase.typed_data(), rfi_phase.dimensions()[0],
       rfi_phase.dimensions()[1], rfi_phase.dimensions()[2],
-      rfi_phase.dimensions()[3],
-      rfi_phase.dimensions()[4] * rfi_phase.dimensions()[5]);
+      rfi_phase.dimensions()[3] * rfi_phase.dimensions()[4] *
+          rfi_phase.dimensions()[5]);
 
-  Tensor5D<const double *, INT_T> rfi_phase_grad_tensor(
+  Tensor4D<const double *, INT_T> rfi_phase_grad_tensor(
       rfi_phase_grad.typed_data(), rfi_phase_grad.dimensions()[0],
       rfi_phase_grad.dimensions()[1], rfi_phase_grad.dimensions()[2],
-      rfi_phase_grad.dimensions()[3],
-      rfi_phase_grad.dimensions()[4] * rfi_phase_grad.dimensions()[5]);
+      rfi_phase_grad.dimensions()[3] * rfi_phase_grad.dimensions()[4] *
+          rfi_phase_grad.dimensions()[5]);
 
   Tensor3D<cuDoubleComplex *, INT_T> rfi_grad_tensor(
       (cuDoubleComplex *)rfi_grad->typed_data(), rfi_grad->dimensions()[0],
@@ -232,35 +218,42 @@ ffi::Error calc_rfi_jvp_gpu_dispatch(
   const INT_T n_int_f = (INT_T)rfi_amp_fine.dimensions()[4];
   const INT_T n_int_t = (INT_T)rfi_amp_fine.dimensions()[5];
 
-  // Cooperative group size. Must be power of 2. Used to iterate over n_int and
-  // reduce result. If 32, equal to warp size on Nvidia for fast reduce
-  // operation.
+  const double scale = 1 / double(n_int_t * n_int_f);
 
-  constexpr int block_size = 256;
   const auto n_time = rfi_grad_tensor.shape[2];
   const auto n_bl = a1.dimensions()[0];
   const auto n_freq = rfi_grad_tensor.shape[1];
 
-  const int warp_size = get_device_prop().warpSize;
+  auto grid = create_clamped_grid(n_time, n_bl, n_freq);
 
-  dim3 block(block_size);
-  auto n_warps = block.x / warp_size;
-
-  auto grid =
-      create_clamped_grid((n_time + n_warps - 1) / n_warps, n_bl, n_freq);
-
-  if (warp_size == 32) {
-    rfi_jvp_kernel<block_size, 32, INT_T><<<grid, block, 0, stream>>>(
-        n_int_f, n_int_t, a1_tensor, a2_tensor, rfi_amp_fine_tensor,
+  if (rfi_phase_tensor.shape[3] / 2 < 32) {
+    constexpr int block_size = 32;
+    dim3 block(block_size);
+    rfi_jvp_kernel<block_size, INT_T><<<grid, block, 0, stream>>>(
+        scale, a1_tensor, a2_tensor, rfi_amp_fine_tensor,
         rfi_amp_fine_grad_tensor, rfi_phase_tensor, rfi_phase_grad_tensor,
         rfi_grad_tensor);
-  } else if (warp_size == 64) {
-    rfi_jvp_kernel<block_size, 64, INT_T><<<grid, block, 0, stream>>>(
-        n_int_f, n_int_t, a1_tensor, a2_tensor, rfi_amp_fine_tensor,
+  } else if (rfi_phase_tensor.shape[3] / 2 < 64) {
+    constexpr int block_size = 64;
+    dim3 block(block_size);
+    rfi_jvp_kernel<block_size, INT_T><<<grid, block, 0, stream>>>(
+        scale, a1_tensor, a2_tensor, rfi_amp_fine_tensor,
+        rfi_amp_fine_grad_tensor, rfi_phase_tensor, rfi_phase_grad_tensor,
+        rfi_grad_tensor);
+  } else if (rfi_phase_tensor.shape[3] / 2 < 128) {
+    constexpr int block_size = 128;
+    dim3 block(block_size);
+    rfi_jvp_kernel<block_size, INT_T><<<grid, block, 0, stream>>>(
+        scale, a1_tensor, a2_tensor, rfi_amp_fine_tensor,
         rfi_amp_fine_grad_tensor, rfi_phase_tensor, rfi_phase_grad_tensor,
         rfi_grad_tensor);
   } else {
-    return ffi::Error::Internal("Unsupported GPU warp size.");
+    constexpr int block_size = 256;
+    dim3 block(block_size);
+    rfi_jvp_kernel<block_size, INT_T><<<grid, block, 0, stream>>>(
+        scale, a1_tensor, a2_tensor, rfi_amp_fine_tensor,
+        rfi_amp_fine_grad_tensor, rfi_phase_tensor, rfi_phase_grad_tensor,
+        rfi_grad_tensor);
   }
 
   const auto status = cudaGetLastError();
