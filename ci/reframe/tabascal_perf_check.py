ec9692ea198fab13559f6f899ea24f6a9f5ef226
Chris Finlay <christopher.finlay@epfl.ch>
2026-06-01 09:25:30 +0200

    Update performance benchmark total runtime. Moving to pure numpy/skyfield in setup removed one-time compile costs reducing total runtime by ~10s (3s just compute_rfi_phase and likely something similar in estimate_rfi_sampling.

--- ci/reframe/tabascal_perf_check.py
+++ ci/reframe/tabascal_perf_check.py
@@ -22,13 +22,13 @@ class TabascalPerfCheck(rfm.RunOnlyRegressionTest):
     _reference_by_variant = {
         "Riemann": {
             "daint:gpu": {
-                "total_runtime": (34.0, -0.25, 0.15, "s"),
+                "total_runtime": (23.0, -0.25, 0.15, "s"),
                 "optimizer_runtime": (10.0, -0.20, 0.15, "s"),
             },
         },
         "RiemannFFI": {
             "daint:gpu": {
-                "total_runtime": (23, -0.25, 0.15, "s"),
+                "total_runtime": (17, -0.25, 0.15, "s"),
                 "optimizer_runtime": (5.2, -0.20, 0.15, "s"),
             },
         },
